a740096320d466fe1871bd067bcba5ff0368c784
ricky <rjbenavides@cpp.edu>
2020-11-30 10:04:16 -0800

    Displaying all post on page

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,7 +2,7 @@ import * as api from "../api";
 
 export const getPosts = () => async (dispatch) => {
   try {
-    const { data } = await api.fetchPosts();
+    const { data } = await api.getPosts();
     dispatch({ type: "FETCH_ALL_POSTS", payload: data });
   } catch (error) {
     console.log(error.message);
