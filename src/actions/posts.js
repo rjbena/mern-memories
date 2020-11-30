ff3afff5bb896423986abe4795f7ed1346ce0928
ricky <rjbenavides@cpp.edu>
2020-11-30 09:07:48 -0800

    Added redux store and ability to create post

---
+++ src/actions/posts.js
@@ -0,0 +1,19 @@
+import * as api from "../api";
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: "FETCH_ALL_POSTS", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+    dispatch({ type: "CREATE_POST", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
---
+++ src/reducers/posts.js
@@ -0,0 +1,12 @@
+const reducer = (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL_POSTS":
+      return action.payload;
+    case "CREATE_POST":
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
